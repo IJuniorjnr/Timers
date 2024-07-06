83d27d450f0ccf11948fa96dde297be687c6478b
IJuniorjnr <149729922+IJuniorjnr@users.noreply.github.com>
2024-07-06 14:36:08 -0300

    Update performance

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
+// Constantes
+const TEMPO_PADRAO = 10;
+const PREFIXO_ID = document.body.getAttribute('data-prefixo');
+const NUM_CRONOMETROS = 45;
+
 class Cronometro {
+    static alarmeSoando = false;
+    static audioContextInitialized = false;
+    static cronometrosEmOvertime = [];
+
     constructor(elementId) {
         this.element = document.getElementById(elementId);
-        this.tempoPadrao = 30;
-        this.tempoInicial = parseInt(localStorage.getItem(`${elementId}-tempoInicial`)) || this.tempoPadrao;
+        this.tempoInicial = parseInt(localStorage.getItem(`${elementId}-tempoInicial`)) || TEMPO_PADRAO;
         this.timestamp = parseInt(localStorage.getItem(`${elementId}-timestamp`)) || null;
         this.emExecucao = false;
         this.corPadrao = "#f4f4f4";
+        this.alarmTriggered = false;
 
-        this.element.style.transition = 'background-color 0.5s ease';
-
+        this.setupElement();
+        this.setupAudio();
         this.atualizarTempoDecorrido();
         this.atualizarInterface();
 
@@ -16,52 +25,27 @@ class Cronometro {
             this.iniciar();
         }
 
-        this.alarmTriggered = false;
-        this.setupAudio();
+        if (this.overtime > 0) {
+            Cronometro.cronometrosEmOvertime.push(this);
+        }
+    }
+
+    setupElement() {
+        this.element.style.transition = 'background-color 0.5s ease';
+        this.tempoElement = this.element.querySelector('.tempo');
     }
 
     setupAudio() {
-        try {
-            this.audio = new Audio('audio/alarm.mp3'); // Substitua pelo caminho real do seu arquivo de áudio
-            this.audio.loop = false; // Faz o áudio tocar em loop
-        } catch (error) {
-            console.error('Erro ao inicializar o áudio:', error);
-        }
-    }
-
-    playAlarmSound() {
-        if (this.audio) {
-            this.audio.play().catch(error => console.error('Erro ao tocar o áudio:', error));
-            this.showNotification();
-        } else {
-            console.error('Áudio não inicializado corretamente');
-        }
-    }
-
-    stopAlarmSound() {
-        if (this.audio) {
-            this.audio.pause();
-            this.audio.currentTime = 0; // Reinicia o áudio para o início
-        }
-    }
-
-    showNotification() {
-        if ("Notification" in window && Notification.permission === "granted") {
-            new Notification("Cronômetro Finalizado", {
-                body: "O tempo do cronômetro acabou!",
-                icon: "/path/to/your/icon.png" // Substitua pelo caminho do seu ícone
-            });
-        }
+        this.audio = new Audio('audio/alarm.mp3');
+        this.audio.loop = true;
     }
 
     atualizarTempoDecorrido() {
         if (this.timestamp) {
-            const now = Date.now() / 1000;
-            const tempoDecorrido = now - this.timestamp;
+            const tempoDecorrido = (Date.now() / 1000) - this.timestamp;
             this.tempo = Math.max(this.tempoInicial - tempoDecorrido, 0);
             this.overtime = Math.max(tempoDecorrido - this.tempoInicial, 0);
 
-            // Verifica se o tempo acabou e toca o alarme
             if (this.tempo <= 0 && !this.alarmTriggered) {
                 this.playAlarmSound();
                 this.alarmTriggered = true;
@@ -77,8 +61,8 @@ class Cronometro {
             this.emExecucao = true;
             if (!this.timestamp) {
                 this.timestamp = Date.now() / 1000;
+                localStorage.setItem(`${this.element.id}-timestamp`, this.timestamp);
             }
-            localStorage.setItem(`${this.element.id}-timestamp`, this.timestamp);
             this.atualizarLoop();
         }
     }
@@ -87,7 +71,7 @@ class Cronometro {
         if (this.emExecucao) {
             this.atualizarTempoDecorrido();
             this.atualizarInterface();
-            setTimeout(() => this.atualizarLoop(), 1000);
+            requestAnimationFrame(() => this.atualizarLoop());
         }
     }
 
@@ -96,15 +80,13 @@ class Cronometro {
     }
 
     reiniciar() {
-        showModal("Deseja realmente reiniciar o cronômetro?", () => {
-            this.resetar();
-        });
+        showModal("Deseja realmente reiniciar o cronômetro?", () => this.resetar());
     }
 
     resetar() {
         this.parar();
-        this.tempoInicial = this.tempoPadrao;
-        this.tempo = this.tempoPadrao;
+        this.tempoInicial = TEMPO_PADRAO;
+        this.tempo = TEMPO_PADRAO;
         this.overtime = 0;
         this.timestamp = null;
         this.alarmTriggered = false;
@@ -121,11 +103,16 @@ class Cronometro {
 
     atualizarTempo() {
         const tempo = this.tempo > 0 ? this.tempo : this.overtime;
-        const horas = Math.floor(tempo / 3600);
-        const minutos = Math.floor((tempo % 3600) / 60);
-        const segundos = Math.floor(tempo % 60);
+        const { horas, minutos, segundos } = this.calcularTempo(tempo);
         const sinal = this.tempo > 0 ? '' : '+';
-        this.element.querySelector('.tempo').textContent = `${sinal}${this.formatarNumero(horas)}:${this.formatarNumero(minutos)}:${this.formatarNumero(segundos)}`;
+        this.tempoElement.textContent = `${sinal}${horas}:${minutos}:${segundos}`;
+    }
+
+    calcularTempo(tempo) {
+        const horas = Math.floor(tempo / 3600).toString().padStart(2, '0');
+        const minutos = Math.floor((tempo % 3600) / 60).toString().padStart(2, '0');
+        const segundos = Math.floor(tempo % 60).toString().padStart(2, '0');
+        return { horas, minutos, segundos };
     }
 
     atualizarCor() {
@@ -136,19 +123,7 @@ class Cronometro {
             this.element.style.color = 'black';
         } else if (this.tempo > 0) {
             const progress = 1 - (this.tempo / this.tempoInicial);
-            let r, g, b;
-            
-            if (progress < 0.5) {
-                r = Math.round(255 * (2 * progress));
-                g = 255;
-                b = 0;
-            } else {
-                r = 255;
-                g = Math.round(255 * (2 - 2 * progress));
-                b = 0;
-            }
-            
-            novaCor = `rgb(${r}, ${g}, ${b})`;
+            novaCor = this.calcularCorGradiente(progress);
             this.element.style.color = 'black';
         } else {
             novaCor = 'black';
@@ -158,146 +133,152 @@ class Cronometro {
         this.element.style.backgroundColor = novaCor;
     }
 
-    formatarNumero(numero) {
-        return numero < 10 ? `0${numero}` : numero;
+    calcularCorGradiente(progress) {
+        const r = Math.round(255 * Math.min(2 * progress, 1));
+        const g = Math.round(255 * Math.min(2 - 2 * progress, 1));
+        return `rgb(${r}, ${g}, 0)`;
     }
 
-    static ordenarCronometros() {
-        const container = document.querySelector('.container');
-        const cronometros = Array.from(container.querySelectorAll('.cronometro')).filter(elem => elem.__cronometro);
-        
-        cronometros.sort((a, b) => {
-            const cronometroA = a.__cronometro;
-            const cronometroB = b.__cronometro;
-            if (cronometroA.tempo === 0 && cronometroB.tempo === 0) {
-                return cronometroB.overtime - cronometroA.overtime;
-            } else if (cronometroA.tempo === 0) {
-                return -1;
-            } else if (cronometroB.tempo === 0) {
-                return 1;
-            } else {
-                return cronometroA.tempo - cronometroB.tempo;
+    playAlarmSound() {
+        if (Cronometro.audioContextInitialized && !Cronometro.alarmeSoando) {
+            Cronometro.alarmeSoando = true;
+            this.audio.play().catch(error => console.error('Erro ao tocar o áudio:', error));
+        } else {
+            console.log('Áudio não inicializado ou alarme já tocando.');
+            if (!Cronometro.cronometrosEmOvertime.includes(this)) {
+                Cronometro.cronometrosEmOvertime.push(this);
             }
+        }
+    }
+
+    stopAlarmSound() {
+        this.audio.pause();
+        this.audio.currentTime = 0;
+        Cronometro.alarmeSoando = false;
+    }
+
+    static ordenarCronometros(cronometros) {
+        return cronometros.sort((a, b) => {
+            if (a.tempo === 0 && b.tempo === 0) return b.overtime - a.overtime;
+            if (a.tempo === 0) return -1;
+            if (b.tempo === 0) return 1;
+            return a.tempo - b.tempo;
         });
-    
-        cronometros.forEach(elem => container.appendChild(elem));
+    }
+
+    static initAudioContext() {
+        if (!Cronometro.audioContextInitialized) {
+            const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+            if (audioContext.state === 'suspended') {
+                audioContext.resume();
+            }
+            Cronometro.audioContextInitialized = true;
+            
+            Cronometro.verificarETocarAlarme();
+        }
+    }
+
+    static verificarETocarAlarme() {
+        if (Cronometro.cronometrosEmOvertime.length > 0 && !Cronometro.alarmeSoando) {
+            Cronometro.cronometrosEmOvertime[0].playAlarmSound();
+        }
     }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    const prefixoId = document.body.getAttribute('data-prefixo');
-    const btnResetarTodos = document.getElementById('btnResetarTodos');
     const cronometros = [];
+    const container = document.querySelector('.container');
 
-    for (let i = 1; i <= 45; i++) {
-        const cronometroId = `${prefixoId}-${i.toString().padStart(2, '0')}`;
+    for (let i = 1; i <= NUM_CRONOMETROS; i++) {
+        const cronometroId = `${PREFIXO_ID}-${i.toString().padStart(2, '0')}`;
         const cronometro = new Cronometro(cronometroId);
         cronometros.push(cronometro);
 
-        const btnIniciar = document.getElementById(`btnIniciar${i}`);
-        btnIniciar.addEventListener('click', () => {
+        document.getElementById(`btnIniciar${i}`).addEventListener('click', () => {
+            Cronometro.initAudioContext();
             cronometro.iniciar();
-            // Enable audio context on user interaction
-            document.getElementById('alarmSound').play().then(() => {
-                document.getElementById('alarmSound').pause();
-                document.getElementById('alarmSound').currentTime = 0;
-            }).catch(error => console.log("Audio play failed:", error));
+            verificarCronometrosOvertime();
         });
 
-        const btnReiniciar = document.getElementById(`btnReiniciar${i}`);
-        btnReiniciar.addEventListener('click', () => cronometro.reiniciar());
+        document.getElementById(`btnReiniciar${i}`).addEventListener('click', () => {
+            Cronometro.initAudioContext();
+            cronometro.reiniciar();
+            verificarCronometrosOvertime();
+        });
     }
 
-    btnResetarTodos.addEventListener('click', function () {
+    document.getElementById('btnResetarTodos').addEventListener('click', () => {
         showModal("Deseja realmente reiniciar todos os cronômetros?", () => {
             cronometros.forEach(cronometro => cronometro.resetar());
+            verificarCronometrosOvertime();
         });
     });
 
-    function ordenarCronometros() {
-        cronometros.sort((a, b) => {
-            if (a.tempo === 0 && b.tempo === 0) {
-                return b.overtime - a.overtime;
-            } else if (a.tempo === 0) {
-                return -1;
-            } else if (b.tempo === 0) {
-                return 1;
-            } else {
-                return a.tempo - b.tempo;
-            }
-        });
-
-        const container = document.querySelector('.container');
+    function ordenarEAtualizarCronometros() {
+        Cronometro.ordenarCronometros(cronometros);
         container.innerHTML = '';
-        cronometros.forEach(cronometro => {
-            container.appendChild(cronometro.element);
+        cronometros.forEach(cronometro => container.appendChild(cronometro.element));
+    }
+
+    function verificarCronometrosOvertime() {
+        Cronometro.cronometrosEmOvertime = cronometros.filter(c => c.overtime > 0);
+        Cronometro.verificarETocarAlarme();
+    }
+
+    setInterval(ordenarEAtualizarCronometros, 1000);
+    setInterval(verificarCronometrosOvertime, 1000);
+
+    ['mostrarPrimeiras21', 'mostrarRestantes', 'mostrarTodos'].forEach(id => {
+        document.getElementById(id).addEventListener('click', () => atualizarVisibilidade(id));
+    });
+
+    function atualizarVisibilidade(modo) {
+        cronometros.forEach((cronometro, index) => {
+            const visivel = (modo === 'mostrarTodos') ||
+                            (modo === 'mostrarPrimeiras21' && index < 21) ||
+                            (modo === 'mostrarRestantes' && index >= 21);
+            cronometro.element.style.display = visivel ? 'inline-block' : 'none';
         });
     }
 
-    setInterval(ordenarCronometros, 1000);
-
-    document.getElementById('mostrarPrimeiras21').addEventListener('click', function () {
-        for (let i = 1; i <= 45; i++) {
-            const cronometroElement = document.getElementById(`${prefixoId}-${i.toString().padStart(2, '0')}`);
-            cronometroElement.style.display = i <= 21 ? 'inline-block' : 'none';
-        }
-    });
-
-    document.getElementById('mostrarRestantes').addEventListener('click', function () {
-        for (let i = 1; i <= 45; i++) {
-            const cronometroElement = document.getElementById(`${prefixoId}-${i.toString().padStart(2, '0')}`);
-            cronometroElement.style.display = i > 21 ? 'inline-block' : 'none';
-        }
-    });
-
-    document.getElementById('mostrarTodos').addEventListener('click', function () {
-        for (let i = 1; i <= 45; i++) {
-            const cronometroElement = document.getElementById(`${prefixoId}-${i.toString().padStart(2, '0')}`);
-            cronometroElement.style.display = 'inline-block';
-        }
-    });
+    // Adiciona um event listener para qualquer clique na página
+    document.addEventListener('click', () => {
+        Cronometro.initAudioContext();
+        verificarCronometrosOvertime();
+    }, { once: true });  // O evento será disparado apenas uma vez
 });
 
 document.addEventListener("visibilitychange", () => {
     if (!document.hidden) {
-        const cronometros = document.querySelectorAll('[id^="cronometro-"]');
-        cronometros.forEach(elem => {
+        document.querySelectorAll(`[id^="${PREFIXO_ID}-"]`).forEach(elem => {
             const cronometro = elem.__cronometro;
-            if (cronometro && cronometro.emExecucao) {
+            if (cronometro?.emExecucao) {
                 cronometro.atualizarTempoDecorrido();
                 cronometro.atualizarInterface();
             }
         });
+        Cronometro.verificarETocarAlarme();
     }
 });
 
 function showModal(message, onConfirm, onCancel) {
     const modal = document.getElementById('customModal');
-    const modalMessage = document.getElementById('modalMessage');
-    const confirmButton = document.getElementById('modalConfirm');
-    const cancelButton = document.getElementById('modalCancel');
-
-    modalMessage.textContent = message;
+    document.getElementById('modalMessage').textContent = message;
     modal.style.display = 'block';
 
-    confirmButton.onclick = () => {
+    document.getElementById('modalConfirm').onclick = () => {
         modal.style.display = 'none';
         if (onConfirm) onConfirm();
     };
 
-    cancelButton.onclick = () => {
+    document.getElementById('modalCancel').onclick = () => {
         modal.style.display = 'none';
         if (onCancel) onCancel();
     };
 }
 
-function hideModal() {
-    document.getElementById('customModal').style.display = 'none';
-}
-
 window.onclick = (event) => {
-    const modal = document.getElementById('customModal');
-    if (event.target == modal) {
-        hideModal();
+    if (event.target === document.getElementById('customModal')) {
+        document.getElementById('customModal').style.display = 'none';
     }
 };
